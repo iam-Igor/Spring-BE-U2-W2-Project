caf1b7e85997825e60a816fd8d045792d156a55f
Ygor Garofalo <igorgarofalo@gmail.com>
2024-01-19 16:37:20 +0100

    fixes

--- src/main/java/ygorgarofalo/SpringU2W2Project/config/CloudinaryConfig.java
+++ src/main/java/ygorgarofalo/SpringU2W2Project/config/CloudinaryConfig.java
@@ -16,7 +16,6 @@ public class CloudinaryConfig {
     public Cloudinary uploadImage(@Value("${cloudinary.name}") String name,
                                   @Value("${cloudinary.apikey}") String apikey,
                                   @Value("${cloudinary.secret}") String secret) {
-
         Map<String, String> config = new HashMap<>();
         config.put("cloud_name", name);
         config.put("api_key", apikey);
